1dd9c491bc173189913d64b37e635d86b7a4361b
artests <illusors1>
2016-01-29 15:16:42 +0300

    Continue4

--- app/src/main/java/com/example/artests/l35ownview/MainActivity.java
+++ app/src/main/java/com/example/artests/l35ownview/MainActivity.java
@@ -3,16 +3,20 @@ package com.example.artests.l35ownview;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     //private MyWidgetView mWidget1, mWidget2, mWidget3;
+    private ShapeView mShapeView;
     private CircleView mWidget1, mWidget2, mWidget3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        TouchCircle touchCircle = new TouchCircle(this);
-        setContentView(touchCircle);
+        setContentView(R.layout.activity_main);
+        mShapeView = (ShapeView) findViewById(R.id.shapeView);
+        //TouchCircle touchCircle = new TouchCircle(this);
+        //setContentView(touchCircle);
         /*mWidget1 = (MyWidgetView)findViewById(R.id.mywidget1);
         mWidget2 = (MyWidgetView)findViewById(R.id.mywidget2);
         mWidget3 = (MyWidgetView)findViewById(R.id.mywidget3);
@@ -26,5 +30,9 @@ public class MainActivity extends AppCompatActivity {
 
         mWidget2.setColor(Color.YELLOW);
         mWidget3.setColor(Color.BLUE);*/
+    }
+    public void onClick(View v) {
+        Toast.makeText(getApplicationContext(), "Вы выбрали: " +
+                mShapeView.getSelectedShape(), Toast.LENGTH_LONG).show();
     }
 }
